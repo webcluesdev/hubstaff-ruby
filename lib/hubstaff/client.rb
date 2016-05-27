b48fb6720ea6cb22ebb817509b4856fc687c3055
Smulligan85 <sean.mulligan85@gmail.com>
2016-05-27 06:57:50 -0700

    Add initialize to Hubstaff::Client

--- lib/hubstaff/client.rb
+++ lib/hubstaff/client.rb
@@ -4,13 +4,20 @@ require 'json'
 
 class Hubstaff::Client
 
+  attr_reader :email, :password
+
+  def initialize(email, password)
+    @email = email
+    @password = password
+  end
+
   def authenticate_client_and_return_auth_token
     @response ||= Faraday.post do |req|
       req.url "https://api.hubstaff.com/v1/auth"
       req.headers['Content-Type'] = 'application/json'
       req.headers['App-Token'] = ENV['APP_TOKEN']
-      req.params['email'] = ENV['APP_EMAIL']
-      req.params['password'] = ENV['APP_PASSWORD']
+      req.params['email'] = self.email
+      req.params['password'] = self.password
     end
     @auth_token = JSON.parse(@response.body)['user']['auth_token']
   end
--- spec/hubstaff/client_spec.rb
+++ spec/hubstaff/client_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Hubstaff
   describe Client do
     before :each do 
-      @client = Hubstaff::Client.new
+      @client = Hubstaff::Client.new(ENV['APP_EMAIL'], ENV['APP_PASSWORD'])
     end
     describe  "#authenticate_client_and_return_auth_token" do
       it "returns the users auth_token" do
