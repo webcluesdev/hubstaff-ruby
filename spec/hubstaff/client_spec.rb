c048ab4ad15fbb8636a0e3f85cae1eb2ee0df558
Smulligan85 <sean.mulligan85@gmail.com>
2016-05-27 07:31:45 -0700

    Final change to #authenticate_client_and_return_auth_token

--- spec/hubstaff/client_spec.rb
+++ spec/hubstaff/client_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 module Hubstaff
   describe Client do
     before :each do 
-      @client = Hubstaff::Client.new(ENV['APP_EMAIL'], ENV['APP_PASSWORD'])
+      @client = Hubstaff::Client.new
     end
     describe  "#authenticate_client_and_return_auth_token" do
       it "returns the users auth_token" do
-        expect(@client.authenticate_client_and_return_auth_token).to eq(ENV['AUTH_TOKEN'])
+        expect(@client.authenticate_client_and_return_auth_token(ENV['APP_EMAIL'], ENV['APP_PASSWORD'])).to eq(ENV['AUTH_TOKEN'])
       end
     end
   end
