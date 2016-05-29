09cecb97501497f127140b8c2e0de4c371eec63c
Smulligan85 <sean.mulligan85@gmail.com>
2016-05-29 12:17:17 -0700

    Add Project module and get methods

---
+++ spec/hubstaff/modules/projects_module_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+class Hubstaff::Client
+  describe Project do
+    before(:all) do
+      @client = Hubstaff::Client.new
+    end
+
+    describe '#projects' do
+      it "returns a collection of projects" do
+        expect(@client.projects['projects']).to be_an_instance_of(Array)
+      end
+    end
+
+    describe '#find_project' do
+      it "returns a specific project" do
+        expect(@client.find_project(112761)).to be_an_instance_of(Hash)
+      end
+    end
+
+    describe '#find_project_members' do
+      it "returns a collection of members for a specific project" do
+        expect(@client.find_project_members(112761)['users']).to be_an_instance_of(Array)
+      end
+    end
+  end
+end
