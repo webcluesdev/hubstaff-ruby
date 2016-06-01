5603e29b6469c8ab82e0f0a3c8bf272a5f2e5106
rkcudjoe <rkcudjoe@gmail.com>
2016-06-01 12:51:42 -0400

    sync hubstaff.rb

--- lib/hubstaff.rb
+++ lib/hubstaff.rb
@@ -1,20 +1,8 @@
 require "hubstaff/version"
 require "hubstaff/client"
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 require 'pry'
 require 'dotenv'
->>>>>>> e7c508f7625e351f54e37de15331ea619032d41d
-=======
-require 'pry'
-require 'dotenv'
->>>>>>> e7c508f7625e351f54e37de15331ea619032d41d
-=======
-require 'pry'
-require 'dotenv'
->>>>>>> 4d9099198ef880fa4b608af9c91ce10107f85be6
 
 module Hubstaff
   # Your code goes here...
