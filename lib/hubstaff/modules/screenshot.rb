ee96e979b87fc535eab3f131407808e3cf801b72
rkcudjoe <rkcudjoe@gmail.com>
2016-09-29 11:11:00 -0400

    update screenshoot module specs

--- lib/hubstaff/modules/screenshot.rb
+++ lib/hubstaff/modules/screenshot.rb
@@ -2,16 +2,14 @@ class Hubstaff::Client
   module Screenshot
 
     def screenshots(start_time, stop_time, options={})
-        screenshot = connection.get("screenshots") do |req|
-          req.params['start_time'] = start_time
-          req.params['stop_time'] = stop_time
-          req.params['organizations'] = options[:orgs] unless options[:orgs].nil?
-          req.params['projects'] = options[:projects] unless options[:projects].nil?
-          req.params['users'] = options[:users] unless options[:users].nil?
-          req.params['offset'] = options[:offset] unless options[:offset].nil?
-        end
-        screenshot_json = JSON.parse(screenshot.body)
-        screenshot_json
+      connection.get("screenshots") do |req|
+        req.params['start_time'] = start_time
+        req.params['stop_time'] = stop_time
+        req.params['organizations'] = options[:orgs] unless options[:orgs].nil?
+        req.params['projects'] = options[:projects] unless options[:projects].nil?
+        req.params['users'] = options[:users] unless options[:users].nil?
+        req.params['offset'] = options[:offset] unless options[:offset].nil?
+      end
     end
   end
 end
--- spec/hubstaff/modules/screenshot_module_spec.rb
+++ spec/hubstaff/modules/screenshot_module_spec.rb
@@ -2,27 +2,30 @@ require 'spec_helper'
 
 class Hubstaff::Client
   describe Screenshot do
-      before(:each) do
+    before(:each) do
       @client = Hubstaff::Client.new(ENV['APP_TOKEN'])
       @client.auth_token = ENV['AUTH_TOKEN']
     end
 
-    describe '#screenshots' do
+describe '#screenshots' do
       it "returns a collection of screenshots for a specific organization" do
-        VCR.use_cassette 'screen/screenshots_org' do
-          expect(@client.screenshots("2016-05-22", "2016-05-25", orgs: "27572")).to be_an_instance_of(Hash)
+        VCR.use_cassette('screen/screenshots_org', re_record_interval: 1000) do
+          req = @client.screenshots("2016-05-22", "2016-05-25", orgs: "27572")
+          expect(req.success?).to be_truthy
         end
       end
 
       it "returns a collection of screenshots for a specific project" do
-        VCR.use_cassette 'screen/screenshots_project' do
-          expect(@client.screenshots("2016-05-22", "2016-05-25", projects: "112761")).to be_an_instance_of(Hash)
+        VCR.use_cassette('screen/screenshots_project', re_record_interval: 1000) do
+          req = @client.screenshots("2016-05-22", "2016-05-25", projects: "112761")
+          expect(req.success?).to be_truthy
         end
       end
 
       it "returns a collection of screenshots for a specific user" do
-        VCR.use_cassette 'screen/screenshots_user' do
-          expect(@client.screenshots("2016-05-22", "2016-05-25", users: "61188")).to be_an_instance_of(Hash)
+        VCR.use_cassette('screen/screenshots_user', re_record_interval: 1000) do
+          req = @client.screenshots("2016-05-22", "2016-05-25", users: "61188")
+          expect(req.success?).to be_truthy
         end
       end
     end
