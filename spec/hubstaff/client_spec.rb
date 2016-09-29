1836a96a42b98d1dbbf4cb5532ff1e138234d2d4
rkcudjoe <rkcudjoe@gmail.com>
2016-09-29 08:43:02 -0400

    updates the activies module

--- spec/hubstaff/client_spec.rb
+++ spec/hubstaff/client_spec.rb
@@ -9,7 +9,7 @@ module Hubstaff
 
     describe  "#authenticate" do
       it "returns the users auth_token" do
-        VCR.use_cassette('api/client', re_record_interval: 10000) do
+        VCR.use_cassette('api/client', re_record_interval: 1000) do
           expect(@client.authenticate(ENV['APP_EMAIL'],ENV["APP_PASSWORD"])).to eq(ENV['AUTH_TOKEN'])
         end
       end
--- spec/hubstaff/modules/task_module_spec.rb
+++ spec/hubstaff/modules/task_module_spec.rb
@@ -9,28 +9,36 @@ class Hubstaff::Client
 
     describe '#tasks' do
       it "returns a collection of tasks" do
-        VCR.use_cassette('task/tasks' do
-          expect(@client.tasks(projects: "112761")).to be_an_instance_of(Hash)
+        VCR.use_cassette('task/tasks',re_record_interval: 10) do
+          req = @client.tasks(projects: "112761")
+          expect(req.success?).to be_truthy
+          #expect(@client.tasks(projects: "112761")).to be_an_instance_of(Hash)
         end
       end
 
       it "returns a collection of tasks for a specific project" do
-        VCR.use_cassette 'task/tasks_project' do
-          expect(@client.tasks(projects: "112761")).to be_an_instance_of(Hash)
+        VCR.use_cassette('task/tasks_project', re_record_interval: 10) do
+          req = @client.tasks(projects:"112761")
+          expect(req.success?).to be_truthy
+          #expect(@client.tasks(projects: "112761")).to be_an_instance_of(Hash)
         end
       end
 
       it "returns a collection of tasks with a offset" do
-        VCR.use_cassette 'task/tasks_offset' do
-          expect(@client.tasks(offset: 0)).to be_an_instance_of(Hash)
+        VCR.use_cassette('task/tasks_offset', re_record_interval: 10)do
+          req = @client.tasks(offset: 0)
+          expect(req.success?).to be_truthy
+          #expect(@client.tasks(offset: 0)).to be_an_instance_of(Hash)
         end
       end
     end
 
     describe '#find_task' do
       it "returns a specific task" do
-        VCR.use_cassette 'task/find_task' do
-          expect(@client.find_task(716530)).to be_an_instance_of(Hash)
+        VCR.use_cassette('task/find_task', re_record_interval: 10) do
+          req = @client.find_task("716530")
+          expect(req.success?).to be_truthy
+          #expect(@client.find_task(716530)).to be_an_instance_of(Hash)
         end
       end
     end
