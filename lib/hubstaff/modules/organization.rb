8dc34edc9ce6eee09bfb8b419f96d0e7d85384c0
rkcudjoe <rkcudjoe@gmail.com>
2016-09-29 11:37:01 -0400

    add update org module

--- lib/hubstaff/modules/organization.rb
+++ lib/hubstaff/modules/organization.rb
@@ -2,32 +2,25 @@ class Hubstaff::Client
   module Organization
 
     def organizations(offset=0)
-      orgs = connection.get("organizations") do |req|
+      connection.get("organizations") do |req|
         req.params['offset'] = offset
       end
-      org_json = JSON.parse(orgs.body)
-      org_json
     end
 
     def find_organization(org_id)
-      found_org = get_json("organizations/#{org_id}")
-      found_org
+      connection.get("organizations/#{org_id}")
     end
 
     def find_org_projects(org_id, offset=0)
-      org_projects = connection.get("organizations/#{org_id}/projects") do |req|
+      connection.get("organizations/#{org_id}/projects") do |req|
         req.params['offset'] = offset
       end
-      org_project_json = JSON.parse(org_projects.body)
-      org_project_json
     end
 
     def find_org_members(org_id, offset=0)
-      org_members = connection.get("organizations/#{org_id}/members") do |req|
+      connection.get("organizations/#{org_id}/members") do |req|
         req.params['offset'] = offset
       end
-      org_member_json = JSON.parse(org_members.body)
-      org_member_json
     end
   end
 end
--- spec/hubstaff/modules/orgs_module_spec.rb
+++ spec/hubstaff/modules/orgs_module_spec.rb
@@ -9,32 +9,36 @@ class Hubstaff::Client
 
     describe '#organizations' do
       it "returns a collection of organizations" do
-        VCR.use_cassette 'org/orgs' do
-          expect(@client.organizations['organizations']).to be_an_instance_of(Array)
+        VCR.use_cassette('org/orgs', re_record_interval: 1000) do
+          req = @client.organizations
+          expect(req.success?).to be_truthy
         end
       end
     end
 
     describe '#find_organization' do
       it "returns a specific organization" do
-        VCR.use_cassette 'org/find_org' do
-          expect(@client.find_organization(27572)).to be_an_instance_of(Hash)
+        VCR.use_cassette('org/find_org', re_record_interval: 1000) do
+          req = @client.find_organization("27572")
+          expect(req.success?).to be_truthy
         end
       end
     end
 
     describe "find_org_projects" do
       it "returns active projects for a specific organization" do
-        VCR.use_cassette 'org/find_org_project' do
-          expect(@client.find_org_projects(27572)).to be_an_instance_of(Hash)
+        VCR.use_cassette('org/find_org_project', re_record_interval: 1000) do
+          req = @client.find_org_projects("27572")
+          expect(req.success?).to be_truthy
         end
       end
     end
 
     describe "find_org_members" do
       it "returns a collection of users that are members of a specific org" do
-        VCR.use_cassette 'org/find_org_members' do
-          expect(@client.find_org_members(27572)['users']).to be_an_instance_of(Array)
+        VCR.use_cassette('org/find_org_members', re_record_interval: 1000) do
+          req = @client.find_org_members("27572")
+          expect(req.success?).to be_truthy
         end
       end
     end
