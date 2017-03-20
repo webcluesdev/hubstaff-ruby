e6c214c3b931a1460c5ff4c190fd8a27b31504ab
rkcudjoe <rkcudjoe@gmail.com>
2017-03-20 05:35:49 -0400

    all tests go

--- example/data_export_example.rb
+++ example/data_export_example.rb
@@ -4,8 +4,12 @@ APP_TOKEN = "1u6rQ6kuB46U8kSYAXRPDaBvvAiB7GPMMxZVLii79KQ"
 APP_EMAIL = "rkcudjoe@hookengine.com"
 APP_PASSWORD = "hookdev001"
 
-@client ||= Hubstaff::Client.new(APP_TOKEN)
+@client = Hubstaff::Client.new(APP_TOKEN)
 
 @client.authenticate(APP_EMAIL,APP_PASSWORD)
 
-p @client.screenshots("2017-03-10", "2017-03-15", projects: "112761")
+puts @client.auth_token
+
+screen_data = @client.screenshots("2017-03-10", "2017-03-15", projects: "112761")
+
+puts screen_data['screenshots']
